5fd91ec1f5dac5fa74ff12548fb423c9dcc36ea4
yanirc1981 <cerzurc@hotmail.com>
2024-08-07 16:36:15 -0300

    Se arreglan enlaces de panel

--- BiotecFront/src/components/Admin/PanelPage.jsx
+++ BiotecFront/src/components/Admin/PanelPage.jsx
@@ -10,7 +10,7 @@ const PanelPage = () => {
       <h1 className="text-3xl font-bold mb-6">Panel de Administración</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <Link
-          to="/panel/clients"
+          to="/panel/clients/create"
           className="bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out transform hover:scale-105 p-6 flex items-center justify-center"
         >
           <div className="text-center">
@@ -19,7 +19,7 @@ const PanelPage = () => {
           </div>
         </Link>
         <Link
-          to="/panel/products"
+          to="/panel/products/create"
           className="bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out transform hover:scale-105 p-6 flex items-center justify-center"
         >
           <div className="text-center">
@@ -34,6 +34,15 @@ const PanelPage = () => {
           <div className="text-center">
             <h2 className="text-xl font-semibold text-blue-500 mb-2">Gestionar Facturación</h2>
             <p className="text-gray-600">Gestiona las facturas y el historial de transacciones.</p>
+          </div>
+        </Link>
+        <Link
+          to="/register"
+          className="bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out transform hover:scale-105 p-6 flex items-center justify-center"
+        >
+          <div className="text-center">
+            <h2 className="text-xl font-semibold text-blue-500 mb-2">Gestionar Administrador</h2>
+            <p className="text-gray-600">Crear nuevo Administrador.</p>
           </div>
         </Link>
       </div>
